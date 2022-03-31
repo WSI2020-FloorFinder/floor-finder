43b6fcfb7204fd6b16f0eecf3310791426f56bc8
Hareem Akram <hakram@hawk.iit.edu>
2022-03-31 17:15:27 -0500

    Added global variables to comments

--- test/scraper.js
+++ test/scraper.js
@@ -1,7 +1,9 @@
+/* global describe, it */
+
 const assert = require('assert');
 
-describe('The scraper library', function() {
-    it('should not mess with truth', function() {
-        assert(true);
+describe('The scraper library', function () {
+  it('should not mess with truth', function () {
+    assert(true);
   });
 });
