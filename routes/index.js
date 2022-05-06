df1672ac12152041baa84f063e4abb2fa3aef68f
Sebastian <saugusth@hawk.iit.edu>
2022-05-05 21:10:15 -0500

    added routes

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,31 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const result = await findOffices(0, 100, 'Michael Paul Galvin Tower');
   console.log(result);
-  res.render('index', { title: 'Floor Finder', result });
+  res.render('../floorFinder/index');
+});
+
+router.get('/floorOne', async (req, res) => {
+  res.render('../floorFinder/floorOne');
+});
+
+router.get('/floorTwo', async (req, res) => {
+  res.render('../floorFinder/floorTwo');
+});
+
+router.get('/floorThree', async (req, res) => {
+  res.render('../floorFinder/floorThree');
+});
+
+router.get('/floorFour', async (req, res) => {
+  res.render('../floorFinder/floorFour');
+});
+
+router.get('/floorSeven', async (req, res) => {
+  res.render('../floorFinder/floorSeven');
+});
+
+router.get('/floorNine', async (req, res) => {
+  res.render('../floorFinder/floorNine');
 });
 
 module.exports = router;
